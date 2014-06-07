3ae5bcb26767ed511e0d538d0cb93220a3c18eba
Albatross <albatross@nosuch.net>
2014-06-07 11:22:04 -0700

    Added test.cpp.

---
+++ test.cpp
@@ -0,0 +1,11 @@
+/*
+ * This software is licensed under the STAHP-PLAYING-WITH-MAH-TOYZ public license.
+ *
+ * It is additionally public domain.
+ */
+
+#include "dson.hpp"
+
+int main() {
+    //TODO: Testing.
+}
