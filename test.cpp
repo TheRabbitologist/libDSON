93b0dd257979a11929b61c2be505b0908b80e2ce
Albatross <albatross@nosuch.net>
2014-06-14 20:38:06 -0700

    Found a test that libDSON failed.
    Will merge into main, then investigate.

--- test.cpp
+++ test.cpp
@@ -9,12 +9,7 @@
 #include <iostream>
 
 int main() {
-    DsonValue* v = parseDsonV2Value("so 42 also such \"foo\" is \"bar\" wow and 13 many");
-    if(v->getEntryType() == ERROR) {
-        std::cout << "FAIL: " << static_cast<DsonError*>(v)->what() << std::endl;
-        return 0;
-    }
-    DsonNumber num(-0.25);
-    num.serialize(std::cout);
+    DsonObject* obj = parseDsonV2Object("such \"bar\" is so 14.2 also yes many , \"foo\" is such \"marco\" is \"polo\" wow wow");
+    obj->serialize(std::cout);
     std::cout << std::endl;
 }
